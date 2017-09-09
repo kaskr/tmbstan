e9a9480fc31448a81eb50f67de9350a716f4771b
Kasper Kristensen <kaskr@dtu.dk>
2017-09-10 00:13:53 +0200

    Make parallel templates work

--- tmbstan/src/ADFun.cpp
+++ tmbstan/src/ADFun.cpp
@@ -3,27 +3,37 @@
 typedef Eigen::Matrix<double,Eigen::Dynamic,1> vector_double;
 
 void tmb_forward(SEXP f, const vector_double &x, vector_double &y) {
-  ADFun<double>* pf;
-  pf = (ADFun<double>*) R_ExternalPtrAddr(f);
-  y = pf->Forward(0, x);
-  // SEXP tag=R_ExternalPtrTag(f);
-  // if(!strcmp(CHAR(tag), "ADFun"))
-  //   return EvalADFunObjectTemplate<ADFun<double> >(f,theta,control);
-  // if(!strcmp(CHAR(tag), "parallelADFun"))
-  //   return EvalADFunObjectTemplate<parallelADFun<double> >(f,theta,control);
+  SEXP tag=R_ExternalPtrTag(f);
+  if(!strcmp(CHAR(tag), "ADFun")) {
+    ADFun<double>* pf;
+    pf = (ADFun<double>*) R_ExternalPtrAddr(f);
+    y = pf->Forward(0, x);
+  } else
+  if(!strcmp(CHAR(tag), "parallelADFun")) {
+    parallelADFun<double>* pf;
+    pf = (parallelADFun<double>*) R_ExternalPtrAddr(f);
+    y = pf->Forward(0, x);
+  } else
+    Rf_error("Unknown function pointer");
 }
 
 void tmb_reverse(SEXP f, const vector_double &x, vector_double &y) {
-  ADFun<double>* pf;
-  pf = (ADFun<double>*) R_ExternalPtrAddr(f);
-  vector_double v(1);
-  v(0) = 1.;
-  y = pf->Reverse(1, v);
-  // SEXP tag=R_ExternalPtrTag(f);
-  // if(!strcmp(CHAR(tag), "ADFun"))
-  //   return EvalADFunObjectTemplate<ADFun<double> >(f,theta,control);
-  // if(!strcmp(CHAR(tag), "parallelADFun"))
-  //   return EvalADFunObjectTemplate<parallelADFun<double> >(f,theta,control);
+  SEXP tag=R_ExternalPtrTag(f);
+  if(!strcmp(CHAR(tag), "ADFun")) {
+    ADFun<double>* pf;
+    pf = (ADFun<double>*) R_ExternalPtrAddr(f);
+    vector_double v(1);
+    v(0) = 1.;
+    y = pf->Reverse(1, v);
+  } else
+  if(!strcmp(CHAR(tag), "parallelADFun")) {
+    parallelADFun<double>* pf;
+    pf = (parallelADFun<double>*) R_ExternalPtrAddr(f);
+    vector_double v(1);
+    v(0) = 1.;
+    y = pf->Reverse(1, v);
+  } else
+    Rf_error("Unknown function pointer");
 }
 
 template<class Type>
