df64b9f0496c218f48b42c3bbc45867caeb319e9
Kasper Kristensen <kaskr@dtu.dk>
2017-09-18 11:45:01 +0200

    Make examples run in less than 5 sec ?

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -3,5 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 Rscript -e 'pkg <- c("rstan", "TMB"); if(!all(pkg%in%installed.packages()))install.packages(pkg)'
 
+## To be removed when new TMB is on CRAN
 git clone https://github.com/kaskr/adcomp
 cd adcomp; make install
+Rscript -e 'library(TMB); library(methods); runExample("simple")'
