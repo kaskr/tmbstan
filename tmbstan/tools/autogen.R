e555ddd96e0982ec53950312a5bfce8bd6b94428
Kasper Kristensen <kaskr@dtu.dk>
2026-03-20 09:40:34 +0100

    Does this fix MacOS builds ? - MacOS doesn't like R_getVar(.,.,FALSE) because it defines FALSE=0 somewhere - Explanation here: https://github.com/RcppCore/Rcpp/issues/1148

--- tmbstan/tools/autogen.R
+++ tmbstan/tools/autogen.R
@@ -92,7 +92,7 @@ if (utils::packageVersion("rstan") < "2.26") {
   pattern <- "names__.clear()"
 }
 replace <- "
-SEXP shortpar_nam = R_getVar(Rf_install(\"shortpar_nam\"), custom_func::R_env, FALSE);
+SEXP shortpar_nam = R_getVar(Rf_install(\"shortpar_nam\"), custom_func::R_env, static_cast<Rboolean>(0));
 names__ = Rcpp::as<std::vector<std::string> >(shortpar_nam);
 return;
 "
@@ -107,7 +107,7 @@ if (utils::packageVersion("rstan") < "2.26") {
 }
 
 replace <- "
-SEXP shortpar_len = R_getVar(Rf_install(\"shortpar_len\"), custom_func::R_env, FALSE);
+SEXP shortpar_len = R_getVar(Rf_install(\"shortpar_len\"), custom_func::R_env, static_cast<Rboolean>(0));
 for(int i=0; i<LENGTH(shortpar_len); i++) {
   std::vector<size_t> dims__;
   dims__.resize(0);
